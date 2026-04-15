30f603849152c6524aa884d289782a40f7b36ce3
LaasYath <67027434+LaasYath@users.noreply.github.com>
2026-04-14 19:08:44 -0500

    feat(mobile): LIFT-49 generate POIs (#32)
    * feat(mobile): generate pois for updated repo ver
    
    * chore(mobile): resolved linter issues
    
    * fix(mobile): update interaction flows for selecting pickup / dropoff
    
    ---------
    
    Co-authored-by: Pranav Pakanati <pranav.pakanati@gmail.com>

---
+++ apps/mobile/src/utils/types/building-category.ts
@@ -0,0 +1,12 @@
+export type BuildingCategory =
+  | "academic"
+  | "recreation"
+  | "housing"
+  | "parking"
+  | "library"
+  | "administrative"
+  | "health"
+  | "arts"
+  | "other";
+
+export default BuildingCategory;
